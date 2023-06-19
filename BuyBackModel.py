226efe8d1ee91620119949768c0e4420503aae88
IsaacHanssen <111169519+IsaacHanssen@users.noreply.github.com>
2023-06-19 14:12:48 +0800

    Add files via upload

--- BuyBackModel.py
+++ BuyBackModel.py
@@ -3,18 +3,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Variables
-households = 202 # No of households
+households = 200 # No of households
 solar = 0.31 # % of solar
 avesolargen = 25 # Average solar generation (kWh)
 aveelecuse = 9 # Average electricity usage between 9-3pm (kWh)
 bateff = 0.9 # Battery round-trip efficiency
 retail = 0.29 # Retail cost of electricity ($)
-buygrid = 0.135 # buy price from battery
+buygrid = 0.27 # buy price from battery
 sellgrid = 0.07 # sell price to battery
 degredation = 0.02 # per year
 lifetime = 10 # yearly
 batterycost = 200 # $ per kWh
 emmissions = 0.6564 # kg
+retailsellgrid = 0
 
 # Functions
 def HouseholdsWithSolar(): # Determines households with solar
@@ -27,10 +28,10 @@ def ActualExcessPower(): # Determines actual power
     return round(TotalExcessPower() * bateff)
 
 def OptimumBatterySize(): # Determines the optimum battery size
-    return int(round(ActualExcessPower(), -3) / 1000) # If you want a set battery size just hash the return and just return your size in MW
+    return 1#int(round(ActualExcessPower(), -3) / 1000) # If you want a set battery size just hash the return and just return your size in MW
 
 def BatteryCost(): # Determines the battery cost
-    return OptimumBatterySize() * 1000 * batterycost
+    return 1600000
 
 def CheckDegredationCapacity(a, iteration):
     if (OptimumBatterySize() * 1000 * (1 - iteration * degredation)) > a:
@@ -49,8 +50,10 @@ def BuybackSaving(): # Creates lists of separate data
     totalcouncil = 0
     ctotalsaving = 0
     em = 0
+    s=0
     totalemissions = []
     accumemissions = []
+    sellerprofit = []
 
     totalcouncilsaving = []
     for i in range(0,lifetime):
@@ -68,36 +71,39 @@ def BuybackSaving(): # Creates lists of separate data
         em = em + CheckDegredationCapacity(ActualExcessPower(), i) * emmissions * 365
         totalemissions.append(CheckDegredationCapacity(ActualExcessPower(), i) * emmissions * 365)
         accumemissions.append(em)
-    return lifetimelist, degradation, savinglistday, savinglistyear, accumalatedsaving, councilsaving, totalcouncilsaving, totalemissions, accumemissions
+        s = s + (sellgrid-retailsellgrid) * CheckDegredationCapacity(ActualExcessPower(), i)*365
+        sellerprofit.append(s)
+    return lifetimelist, degradation, savinglistday, savinglistyear, accumalatedsaving, councilsaving, totalcouncilsaving, totalemissions, accumemissions, sellerprofit
 
 def PrintBuyBack(): # Prints the data in a DataFrame Structure
-    lifetimelist, degradation, savinglistday, savinglistyear, accumalatedsaving, councilsaving, totalcouncilsaving, totalemissions, accumemissions = BuybackSaving()
+    lifetimelist, degradation, savinglistday, savinglistyear, accumalatedsaving, councilsaving, totalcouncilsaving, totalemissions, accumemissions, sellerprofit = BuybackSaving()
     df = pd.DataFrame({
     'Year':lifetimelist,
     'Degradation':degradation,
-    'Com Savings Daily ($)':savinglistday,
     'Com Savings Yearly ($)':savinglistyear,
     'Accum Com Saving ($)':accumalatedsaving,
+    'Accum Seller Profit ($)':sellerprofit,
     'Council Saving ($)':councilsaving,
     'Accum Council Savings ($)':totalcouncilsaving,
-    'Total Emissions Yearly (kg)':totalemissions,
     'Accumalated Emissions (kg)':accumemissions
     })
     return print(df)
 
 def GraphSavings(): 
-    lifetimelist, degradation, savinglistday, savinglistyear, accumalatedsaving, councilsaving, totalcouncilsaving, totalemissions, accumemissions = BuybackSaving()
-    plt.plot(lifetimelist, totalcouncilsaving, label="Total Council Savings (Accum Yearly)")
-    plt.plot(lifetimelist, accumalatedsaving, label="Total Community Savings (Yearly)")
+    lifetimelist, degradation, savinglistday, savinglistyear, accumalatedsaving, councilsaving, totalcouncilsaving, totalemissions, accumemissions, sellerprofit = BuybackSaving()
+    plt.plot(num=None, figsize=(16, 20), dpi=80, facecolor='w', edgecolor='k')
+    plt.plot(lifetimelist, totalcouncilsaving, label="Private Company Revenue ($)")
+    plt.plot(lifetimelist, accumalatedsaving, label="Buyers Benefits ($)")
     plt.plot(lifetimelist, np.full(lifetimelist[-1], BatteryCost()), label = 'Battery Cost ($)')
-    plt.xlabel("Lifetime of Battery (Years)")
-    plt.ylabel("Savings ($)")
-    plt.legend(loc='upper left')
+    plt.plot(lifetimelist, sellerprofit, label = 'Sellers Benefits ($)')
+    plt.xlabel("Lifetime of Battery (Years)", fontsize = '8')
+    plt.ylabel("Savings ($)", fontsize = '8')
+    plt.legend(loc='upper left', fontsize = '8')
     plt.title("Accumalated Savings Yearly")
     plt.show()
 
 def GraphSavingsSave(): 
-    lifetimelist, degradation, savinglistday, savinglistyear, accumalatedsaving, councilsaving, totalcouncilsaving, totalemissions, accumemissions = BuybackSaving()
+    lifetimelist, degradation, savinglistday, savinglistyear, accumalatedsaving, councilsaving, totalcouncilsaving, totalemissions, accumemissions, sellerprofit = BuybackSaving()
     plt.plot(lifetimelist, totalcouncilsaving, label="Total Council Savings (Accum Yearly)")
     plt.plot(lifetimelist, accumalatedsaving, label="Total Community Savings (Yearly)")
     plt.plot(lifetimelist, np.full(lifetimelist[-1], BatteryCost()), label = 'Battery Cost ($)')
@@ -108,7 +114,7 @@ def GraphSavingsSave():
     plt.savefig("graph.png")
 
 def GraphEnvironment():
-    lifetimelist, degradation, savinglistday, savinglistyear, accumalatedsaving, councilsaving, totalcouncilsaving, totalemissions, accumemissions = BuybackSaving()
+    lifetimelist, degradation, savinglistday, savinglistyear, accumalatedsaving, councilsaving, totalcouncilsaving, totalemissions, accumemissions, sellerprofit = BuybackSaving()
     plt.plot(lifetimelist, totalemissions, label="Total Emissions ( Yearly)")
     plt.plot(lifetimelist, accumemissions, label="Total Accum Emissions (Yearly)")
     plt.xlabel("Lifetime of Battery (Years)")
@@ -118,7 +124,7 @@ def GraphEnvironment():
     plt.show()
 
 def GraphEnvironmentSave():
-    lifetimelist, degradation, savinglistday, savinglistyear, accumalatedsaving, councilsaving, totalcouncilsaving, totalemissions, accumemissions = BuybackSaving()
+    lifetimelist, degradation, savinglistday, savinglistyear, accumalatedsaving, councilsaving, totalcouncilsaving, totalemissions, accumemissions, sellerprofit = BuybackSaving()
     plt.plot(lifetimelist, totalemissions, label="Total Emissions ( Yearly)")
     plt.plot(lifetimelist, accumemissions, label="Total Accum Emissions (Yearly)")
     plt.xlabel("Lifetime of Battery (Years)")
@@ -128,7 +134,7 @@ def GraphEnvironmentSave():
     plt.savefig("graphenvironment.png")
 
 def Analyse(): # Analyses the results, prints the data, and prints statements about the battery
-    lifetimelist, degradation, savinglistday, savinglistyear, accumalatedsaving, councilsaving, totalcouncilsaving, totalemissions, accumemissions = BuybackSaving()
+    lifetimelist, degradation, savinglistday, savinglistyear, accumalatedsaving, councilsaving, totalcouncilsaving, totalemissions, accumemissions, sellerprofit = BuybackSaving()
     PrintBuyBack()
     def PayBack():
         for i in range(0, len(accumalatedsaving)):
@@ -144,7 +150,7 @@ def Analyse(): # Analyses the results, prints the data, and prints statements ab
     print(f"A total of ${round(accumalatedsaving[-1])} will be saved across the lifetime of {lifetime} years of the battery and into the community.")
 
 def DataFrametoCSV():
-    lifetimelist, degradation, savinglistday, savinglistyear, accumalatedsaving, councilsaving, totalcouncilsaving, totalemissions, accumemissions = BuybackSaving()
+    lifetimelist, degradation, savinglistday, savinglistyear, accumalatedsaving, councilsaving, totalcouncilsaving, totalemissions, accumemissions, sellerprofit = BuybackSaving()
     df = pd.DataFrame({
     'Year':lifetimelist,
     'Degradation':degradation,
@@ -154,7 +160,8 @@ def DataFrametoCSV():
     'Council Saving ($)':councilsaving,
     'Accum Council Savings ($)':totalcouncilsaving,
     'Total Emissions Yearly (kg)':totalemissions,
-    'Accumalated Emissions (kg)':accumemissions
+    'Accumalated Emissions (kg)':accumemissions,
+    'Sellers Profit Accumalated ($)':sellerprofit
     })
     df.to_csv("DataFrame.csv")
 
@@ -162,6 +169,6 @@ PrintBuyBack()
 #Analyse()
 #GraphSavings()
 #GraphSavingsSave()
-#GraphEnvironment()
+GraphEnvironment()
 #GraphEnvironmentSave()
 #DataFrametoCSV()
